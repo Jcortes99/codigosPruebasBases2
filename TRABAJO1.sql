5558325e7f78b26d93ec8c1b20e0d89711537631
fulqjulio <juliancarvajal93f@gmail.com>
2023-03-18 01:31:44 -0500

    one almost done

---
+++ TRABAJO1.sql
@@ -0,0 +1,51 @@
+
+-- PRIMER PUNTO TRIGGER CON EXCEPTIONS (25%)
+
+CREATE OR REPLACE TRIGGER CONTROL_DATOEV
+BEFORE INSERT
+  ON evento
+  FOR EACH ROW
+DECLARE
+    cont NUMBER(2) := 0;
+BEGIN
+    
+    FOR pelea IN (SELECT xt.*
+                    FROM XMLTABLE('/Evento/Peleas/Pelea'
+                            PASSING :NEW.datoev
+                            COLUMNS 
+                            pas1     NUMBER(20) PATH 'Pas1',
+                            pas2     NUMBER(20) PATH 'Pas2',
+                            ganador  VARCHAR2(100) PATH 'Ganador',
+                            tecnica  VARCHAR2(100) PATH 'Tecnica'
+                            ) xt) LOOP
+
+        cont := cont + 1;
+
+        IF cont > 5 THEN 
+            raise_application_error(-20005, 'Hay más de 5 peleas.');
+        END IF;
+
+        IF pelea.pas1 = pelea.pas2 THEN
+            raise_application_error(-20001, 'Hay peleador consigo mismo.');
+        END IF;
+
+        IF (pelea.ganador = 0 AND pelea.tecnica IS NOT NULL) THEN
+            raise_application_error(-20002, 'Hay empate con técnica');
+        END IF;
+
+        IF (pelea.ganador != 0 AND pelea.tecnica IS NULL) THEN
+            raise_application_error(-20003, 'Hay ganador sin técnica');
+        END IF;
+    END LOOP;
+
+    IF cont < 1 THEN 
+        raise_application_error(-20004, 'Hay menos de una pelea.');
+    END IF;
+
+END;
+/
+
+-- Se debe de usar el Rise application error para parar la ejecución de la inserción, si se hace con excepciones propias y se catchean de igual forma se ejecuta el insert.
+
+delete from evento where code=991;
+                 
---
+++ anexo.sql
@@ -0,0 +1,121 @@
+DROP TABLE karateca;
+CREATE TABLE karateca(
+  pasaporte NUMBER(20) PRIMARY KEY,
+  datoka XMLTYPE);
+
+INSERT INTO karateca VALUES 
+(55, XMLTYPE('<Karateca>  
+               <Nombre>Paula Abdul</Nombre>
+               <Nickname>Knocked Out</Nickname>
+             </Karateca>'));
+
+INSERT INTO karateca VALUES 
+(66, XMLTYPE('<Karateca>  
+               <Nombre>Rhian Teasdale</Nombre>
+               <Nickname>Wet Leg</Nickname>
+             </Karateca>'));
+
+INSERT INTO karateca VALUES 
+(29, XMLTYPE('<Karateca>  
+               <Nombre>Ariana Grande</Nombre>
+               <Nickname>Mor</Nickname>
+             </Karateca>'));
+
+DROP TABLE evento;
+CREATE TABLE evento(
+  code NUMBER(8) PRIMARY KEY,
+  datoev XMLTYPE);
+
+INSERT INTO evento VALUES 
+(991, XMLTYPE('<Evento>  
+               <Fecha>30/01/2023</Fecha>
+               <Nombre>Kick that B88ch</Nombre>
+               <Peleas>
+                 <Pelea>
+                 <Pas1>55</Pas1>
+                 <Pas2>66</Pas2>
+                 <Ganador>0</Ganador>
+                 </Pelea>
+                 <Pelea>
+                 <Pas1>55</Pas1>
+                 <Pas2>29</Pas2>
+                 <Ganador>1</Ganador>
+                 <Tecnica>Mataleon fulminante</Tecnica>
+                 </Pelea>
+                  <Pelea></Pelea>
+                  <Pelea></Pelea>
+                  <Pelea></Pelea>
+                  <Pelea></Pelea>
+                  <Pelea></Pelea>
+               </Peleas>
+             </Evento>'));
+
+------------------------------------
+
+DROP TABLE peleador;
+CREATE TABLE peleador(
+  pasaporte  NUMBER(20) PRIMARY KEY,
+  datope JSON NOT NULL
+);
+
+INSERT INTO peleador VALUES (55,
+'{
+  "nombre": "Paula Adul",
+  "nickname": "Knocked out",
+  "peleas": [
+    {
+      "fecha": "25/01/2023",
+      "pasrival": 500,
+      "ganador": 0
+    },
+    {
+      "fecha": "26/01/2023",
+      "pasrival": "666",
+      "ganador": 1,
+      "tecnica": "Llave del dragon"     
+    }
+  ]
+}'
+);
+
+INSERT INTO peleador VALUES (500,
+'{
+  "nombre": "Cathy Dennis",
+  "nickname": "Touch me",
+  "peleas": [
+    {
+      "fecha": "25/01/2023",
+      "pasrival": 55,
+      "ganador": 0
+    }
+  ]
+}'
+);
+
+INSERT INTO peleador VALUES (666,
+'{
+  "nombre": "Charli XCX",
+  "nickname": "Boom Clap",
+  "peleas": [
+    {
+      "fecha": "26/01/2023",
+      "pasrival": 55,
+      "ganador": 2,
+      "tecnica": "Llave del dragon"  
+    }
+  ]
+}'
+);
+
+
+--Note que en la base de datos JSON hay cierta redundancia en cuanto a las peleas:
+--La información de cada pelea se repite dos veces (una vez en cada peleador), 
+--pero en la base de datos relacional la pelea se almacenará una sola vez. 
+--Se asegura que los datos de las peleas son consistentes en cuanto a contrincantes 
+--(por ejemplo, que existan sus pasaportes), fecha, resultado y técnica.
+
+
+
+
+
+
